b7e738b07b18973d91956588e7eefde09af43dc9
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-27 11:40:25 +0900

    bug fix

--- TokyoOSS/ViewModels/LoginViewModel.swift
+++ TokyoOSS/ViewModels/LoginViewModel.swift
@@ -52,20 +52,7 @@ final class LoginViewModel:LoginViewModelType,LoginViewModelInputs,LoginViewMode
         doneRegisterTap.emit(onNext: { [weak self] _ in
             self?.outputs.toRegister.accept(())
         }).disposed(by: disposeBag)
-
-
     }
 }
 
-struct Validator {
-    static func isEnableEmail(email: String) -> Bool {
-        let args = "[A-Z0-9a-z._+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let validation = NSPredicate(format: "SELF MATCHES %@", args)
-        return validation.evaluate(with: email)
-    }
-    static func isEnablePassWord(password: String)-> Bool {
-        let args = "^(?=.*?[A-Za-z])(?=.*?[0-9])[A-Za-z0-9]{8,}$"
-        let validation = NSPredicate(format: "SELF MATCHES %@", args)
-        return validation.evaluate(with: password)
-    }
-}
+
