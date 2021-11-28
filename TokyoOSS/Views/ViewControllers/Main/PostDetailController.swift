a609b79f9fd883d8edb3d8be1176006dfa65612c
Katopan <76249494+katomizuki@users.noreply.github.com>
2021-11-28 16:00:55 +0900

    bug fix

--- TokyoOSS/Views/ViewControllers/Main/PostDetailController.swift
+++ TokyoOSS/Views/ViewControllers/Main/PostDetailController.swift
@@ -1,10 +1,27 @@
 import UIKit
 
 class PostDetailController: UIViewController {
-
+    @IBOutlet weak var likeImageView: UIImageView!
+    @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var postDayLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postContentLabel: UITextView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    private var isLiked = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
- 
+    @IBAction func didTapLikeImageView(_ sender: Any) {
+        print(#function)
+        isLiked.toggle()
+        if isLiked {
+            likeImageView.image = UIImage(systemName: "heart.fill")
+        } else {
+            likeImageView.image = UIImage(systemName: "heart")
+        }
+    }
+    
 }
